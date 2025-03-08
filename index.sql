f76ea9084b9ffd655be0347153357aa2909fd928
MiguelDominguez <miguel.dominguez.developer@gmail.com>
2025-03-08 14:40:40 +0100

    15. BETWEEN

--- index.sql
+++ index.sql
@@ -315,3 +315,45 @@ LIMIT 5;
 
 SELECT COUNT(title) FROM film
 WHERE length <= 50;
+
+-- 15. BETWEEN
+-- -- -- The BETWEEN operator is the same as:
+-- -- -- value >= low AND value <= high 
+-- -- -- value BETWEEN low AND high
+
+-- -- -- You can also combine BETWEEN with the NOT logical operator:
+-- -- -- -- value NOT BETWEEN low AND high
+
+-- -- -- The NOT BETWEEN operator is the same as:
+-- -- -- -- value < low OR value > high
+-- -- -- -- value NOT BETWEEN low AND high
+
+-- -- -- The BETWEEN operator can also be used
+-- -- -- with dates. Note that you need to format
+-- -- -- dates in the ISO 8601 standard format,
+-- -- -- YYYY-MM-DD
+-- -- -- -- AND '2007-02-01'
+
+-- -- -- -- When using BETWEEN operator with dates
+-- -- -- -- that also include timestamp information,
+-- -- -- -- pay careful attention to using BETWEEN
+-- -- -- -- versus <=,>= comparison operators, due to 
+-- -- -- -- the fact a datetime starts at 0:00.
+--
+-- -- -- -- Later on we will study more specific 
+-- -- -- -- methods for datetime information types
+
+SELECT * FROM payment
+LIMIT 2;
+
+SELECT * FROM payment
+WHERE amount BETWEEN 8 AND 9;
+
+SELECT COUNT(*) FROM payment 
+WHERE amount BETWEEN 8 AND 9;
+
+SELECT COUNT(*) FROM payment
+WHERE amount NOT BETWEEN 8 AND 9;
+
+SELECT * FROM payment 
+WHERE payment_date BETWEEN '2007-02-01' AND '2007-02-15';
