3e6ae3faa3de911946d2a5972834b12a0a0c7dcc
MiguelDominguez <miguel.dominguez.developer@gmail.com>
2025-03-08 08:50:24 +0100

    First commit

---
+++ index.sql
@@ -0,0 +1,10 @@
+-- SQL Statement Fundamentals
+-- SELECT
+-- SELECT column_name FROM table_name
+SELECT * FROM actor;
+SELECT first_name FROM actor;
+SELECT first_name,last_name FROM actor;
+SELECT last_name,first_name FROM actor;
+select last_name,first_name from actor;
+SELECT last_name,first_name FROM actor;
+SELECT * FROM city;
