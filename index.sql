5a29148975626b1d4bc64d645dac802eb1c8c4fe
MiguelDominguez <miguel.dominguez.developer@gmail.com>
2025-03-08 09:51:48 +0100

    6. SELECT DISTINCT

--- index.sql
+++ index.sql
@@ -20,3 +20,23 @@ SELECT * FROM city;
 -- -- -- You could also use SELECT * FROM customer to see all the columns.
 -- -- Solution
 SELECT first_name,last_name,email FROM customer;
+
+-- 6. SELECT DISTINCT
+-- -- Sometimes a table contains a column that has duplicate values,
+-- -- and you may find yourself in a situation where you only 
+-- -- want to list the unique/distinct values.
+--
+-- -- The DISTINCT keyword can be used to
+-- -- return only the distinct values in a column.
+--
+-- -- The DISTINCT keyword operates 'on' a column.
+-- -- The syntax looks like this:
+SELECT DISTINCT column FROM table;
+-- -- It will work with or without parenthesis.
+-- -- Later on when we learn about adding more
+-- -- calls such as COUNT and DISTINCT 
+-- -- together, the parenthesis will be necessary.
+SELECT * FROM film;
+SELECT DISTINCT release_year FROM film;
+SELECT DISTINCT (release_year) FROM film;
+SELECT DISTINCT (rental_rate) FROM film;
