24e0d3384b4ab038ceb6b351e6ab3fdfa6f5e126
MiguelDominguez <miguel.dominguez.developer@gmail.com>
2025-03-08 12:45:20 +0100

    11. Challenge SELECT WHERE

--- index.sql
+++ index.sql
@@ -169,3 +169,52 @@ WHERE rating != 'R';
 
 SELECT * FROM film 
 WHERE rating != 'R';
+
+-- 11. Challenge SELECT WHERE
+-- -- --  Challenge No. 1
+-- -- -- -- A customer forgot thier wallet at our store!
+-- -- -- -- We need to track down their email to inform them.
+-- -- -- -- What is the email for the customer with 
+-- -- -- -- the name Nancy Thomas?
+
+-- -- Hints for Challenge No. 1
+-- -- -- Use the customer table
+-- -- -- Make sure the capitalization and spelling
+-- -- -- of the names si correct
+-- -- -- Use single quotes around the 'string'
+
+SELECT email FROM customer 
+WHERE first_name = 'Nancy'
+AND last_name = 'Thomas'; 
+
+-- -- -- Challenge No. 2
+-- -- -- -- A customer wants to know what the 
+-- -- -- -- movie "Outlaw Hanky" is about.
+-- -- -- -- Could you give them the description for 
+-- -- -- -- the movie "Outlaw Hanky"?
+
+-- -- Hints for Challenge No. 2
+-- -- -- Use the film table
+-- -- -- Make sure the capitalization and spelling 
+-- -- -- of the movie name is correct 
+-- -- -- Use single quotes around the 'string'
+
+SELECT description FROM film 
+WHERE title = 'Outlaw Hanky';
+
+-- -- -- Challenge No. 3
+-- -- -- -- A customer is late on their movie return,
+-- -- -- -- and we've mailed them a letter to their
+-- -- -- -- address at '259 Ipoh Drive'. 
+-- -- -- -- We should also call them on the phone to let them know.
+-- -- -- -- Can you get the phone number for the
+-- -- -- -- customer who lives at '259 Ipoh Drive'? 
+
+-- -- Hints for Challenge No. 3
+-- -- -- -- Use the address table
+-- -- -- -- Make sure the capitalization and spelling 
+-- -- -- -- of the address is correct
+-- -- -- -- Use single quotes around the 'string'
+
+SELECT phone FROM address
+WHERE address = '259 Ipoh Drive';
