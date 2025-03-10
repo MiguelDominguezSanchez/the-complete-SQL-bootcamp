4ba1128b773d81f3a6e445e9424b9ef97f5f3394
MiguelDominguez <miguel.dominguez.developer@gmail.com>
2025-03-10 11:55:32 +0100

    3.GROUP BY Statements - 4. GROUP BY - Part Two

--- 3-Group_By_Statements.sql
+++ 3-Group_By_Statements.sql
@@ -26,4 +26,42 @@ SELECT *
 FROM film;
 --
 SELECT SUM(replacement_cost)
-FROM film;
+FROM film;d
+
+-- -- -- 4. GROUP BY
+SELECT * FROM payment;
+--
+SELECT customer_id FROM payment
+GROUP BY customer_id
+ORDER BY customer_id
+--
+SELECT customer_id,SUM(amount) FROM payment
+GROUP BY customer_id
+--
+SELECT customer_id, COUNT(amount) FROM payment
+GROUP BY customer_id
+ORDER BY COUNT(amount) DESC;
+-- 
+SELECT customer_id,staff_id,SUM(amount) FROM payment
+GROUP BY staff_id,customer_id;
+--
+SELECT staff_id,customer_id,SUM(amount) FROM payment
+GROUP BY staff_id,customer_id
+ORDER BY customer_id
+-- 
+SELECT staff_id,customer_id,SUM(amount) FROM payment
+GROUP BY staff_id,customer_id
+ORDER BY staff_id,customer_id
+--
+SELECT staff_id,customer_id,SUM(amount) FROM payment
+GROUP BY staff_id,customer_id
+ORDER BY(amount)
+--
+SELECT DATE(payment_date) FROM payment
+-- 
+SELECT DATE(payment_date) FROM payment
+GROUP BY DATE(payment_date)
+--
+SELECT DATE(payment_date),SUM(amount) FROM payment
+GROUP BY DATE(payment_date)
+ORDER BY SUM(amount) DESC
