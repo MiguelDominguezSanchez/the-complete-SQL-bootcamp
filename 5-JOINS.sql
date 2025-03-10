c693a2e7c25dde265fab2efb05e74c629bbcddb6
MiguelDominguez <miguel.dominguez.developer@gmail.com>
2025-03-10 17:54:50 +0100

    5. JOINS - 9. JOIN Challenge Tasks

--- 5-JOINS.sql
+++ 5-JOINS.sql
@@ -80,3 +80,65 @@ SELECT film.film_id,title,inventory_id,store_id
 FROM film
 LEFT JOIN inventory ON
 inventory.film_id = film.film_id
+
+---
+
+-- -- -- 9. JOIN Challenge Tasks
+
+SELECT * FROM customer
+
+--
+
+SELECT * FROM address
+
+--
+
+SELECT * FROM address 
+INNER JOIN customer
+ON address.address_id = customer_id
+
+--
+
+SELECT district,email FROM address 
+INNER JOIN customer
+ON address.address_id = customer.address_id
+WHERE district = 'California'
+
+-- 
+
+SELECT district,email FROM customer 
+INNER JOIN address 
+ON address.address_id = customer.address_id
+WHERE district = 'California'
+
+-- 
+
+SELECT * FROM actor
+INNER JOIN film_actor 
+ON actor.actor_id = film_actor.actor_id
+
+--
+
+SELECT * FROM actor 
+INNER JOIN film_actor 
+ON actor.actor_id = film_actor.actor_id
+INNER JOIN film 
+ON film_actor.film_id = film.film_id
+
+-- 
+
+SELECT title,first_name,last_name FROM actor 
+INNER JOIN film_actor
+ON actor.actor_id = film_actor.actor_id
+INNER JOIN film
+ON film_actor.film_id = film.film_id
+
+-- 
+
+SELECT title,first_name,last_name FROM actor
+INNER JOIN film_actor
+ON actor.actor_id = film_actor.actor_id
+INNER JOIN film
+ON film_actor.film_id = film.film_id
+WHERE first_name = 'Nick' AND
+last_name = 'Wahlberg'
