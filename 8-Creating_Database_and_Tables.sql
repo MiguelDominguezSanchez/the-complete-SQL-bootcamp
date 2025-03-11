ad173668eac1774e9edab40d9a68a728e91d5cd9
MiguelDominguez <miguel.dominguez.developer@gmail.com>
2025-03-11 16:16:57 +0100

    8. Creating Database and Tables - 7. UPDATE

--- 8-Creating_Database_and_Tables.sql
+++ 8-Creating_Database_and_Tables.sql
@@ -174,3 +174,58 @@ SELECT * FROM account_job
 INSERT INTO account_job(user_id,job_id,hire_date)
 VALUES 
 (10,10,CURRENT_TIMESTAMP)
+
+-- -- -- 
+
+-- -- -- 7. Update
+
+-- -- -- 
+
+SELECT * FROM account
+
+-- -- -- 
+
+UPDATE account 
+SET last_login = created_on
+
+-- -- -- 
+
+SELECT * FROM account
+
+-- -- -- 
+
+SELECT * FROM job
+
+--
+
+SELECT * FROM account_job
+
+-- 
+
+UPDATE account_job
+SET hire_date = account.create_on 
+FROM account
+WHERE account_job.user_id = account.user_id
+
+--
+
+SELECT * FROM account_job
+
+--
+
+SELECT * FROM account
+
+--
+
+UPDATE account_job 
+SET hire_date = account.created_on
+FROM account 
+WHERE account_job.user_id = account.user_id
+
+--
+
+UPDATE account
+SET last_login = CURRENT_TIMESTAMP
+RETURNING email,created_on,last_login
+
+--
