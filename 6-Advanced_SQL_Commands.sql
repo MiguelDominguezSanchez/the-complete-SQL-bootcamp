9909e48a50547f6ca61889ac007d111f309fdce1
MiguelDominguez <miguel.dominguez.developer@gmail.com>
2025-03-11 11:47:41 +0100

    6. Advanced SQL Commands - 8. SubQuery

--- 6-Advanced_SQL_Commands.sql
+++ 6-Advanced_SQL_Commands.sql
@@ -246,3 +246,67 @@ FROM customer
 SELECT LOWER(LEFT(first_name,1)) || LOWER(last_name) || '@gmail.com'
 AS customer_email
 FROM customer
+
+-- -- -- 
+
+-- -- -- 8. SubQuery
+
+-- -- -- 
+
+SELECT AVG(rental_rate) FROM film
+
+--
+
+SELECT title,rental_rate
+FROM film
+WHERE rental_rate > 
+(SELECT AVG(rental_rate) FROM film)
+
+--
+
+SELECT * FROM rental 
+
+-- 
+
+SELECT * FROM inventory
+
+--
+
+SELECT * FROM rental 
+WHERE return_date BETWEEN '2005-05-29' AND '2005-05-30'
+
+-- 
+
+SELECT inventory.film_id 
+FROM rental
+INNER JOIN inventory ON inventory ON inventory_id = rental.inventory_id
+WHERE return_date BETWEEN '2005-05-29' AND '2005-05-30'
+
+--
+
+SELECT film_id,title
+FROM film
+WHERE film_id IN
+(SELECT inventory.film_id)
+FROM rental
+INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+WHERE return_date BETWEEN '2005-05-29' AND '2005-05-30'
+ORDER BY title
+
+--
+
+SELECT first_name,last_name 
+FROM customer AS c
+WHERE EXISTS
+(SELECT * FROM payment as p
+WHERE p.customer_id = c.customer_id
+AND amount > 11)
+
+-- 
+
+SELECT first_name,last_name
+FROM customer AS c
+WHERE NOT EXISTS 
+(SELECT * FROM payment as p 
+WHERE p.customer_id = c.customer_id
+AND amount > 11)
