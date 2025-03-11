724601252425cbfb7d51fff39106a97497a9ce46
MiguelDominguez <miguel.dominguez.developer@gmail.com>
2025-03-11 07:49:04 +0100

    2. Timestamps and Extract - Part One

---
+++ 6-Advanced_SQL_Commands.sql
@@ -0,0 +1,39 @@
+-- -- -- 2. Timestamps and Extract - Part One
+-- -- -- -- TIME - Contains only time
+-- -- -- -- DATE - Contains only date
+-- -- -- -- TIMESTAMP - Contains date and time
+-- -- -- -- TIMESTAMPTZ - Contains date, time, and timezone 
+--
+-- -- -- Functions and operations related to these specific data types:
+-- -- -- -- TIMEZONE
+-- -- -- -- NOW
+-- -- -- -- TIMEOFDAY
+-- -- -- -- CURRENT_TIME
+-- -- -- -- CURRENT_DATE
+
+--
+
+SHOW ALL 
+
+-- 
+
+SHOW TIMEZONE
+
+--
+
+SELECT NOW()
+
+--
+
+SELECT TIMEOFDAY() 
+
+--
+
+SELECT CURRENT_TIME
+
+--
+
+SELECT CURRENT_DATE
+
+-- 
+
