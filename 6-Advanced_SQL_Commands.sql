31889ea2379fc6d9cba1f34c62887b2fde2cc1cc
MiguelDominguez <miguel.dominguez.developer@gmail.com>
2025-03-11 12:52:27 +0100

    7. Assessment Test 2 - 2. Setting up New Database for Assessment Exercises

--- 6-Advanced_SQL_Commands.sql
+++ 6-Advanced_SQL_Commands.sql
@@ -371,3 +371,5 @@ FROM film AS f1
 INNER JOIN film AS f2 ON 
 f1.film_id != f2.film_id 
 AND f1.length = f2.length
+
+---
---
+++ 7-Assessment_Test_2.sql
@@ -0,0 +1,22 @@
+-- -- -- 2. Setting up New Database for Assessment Exercises
+
+-- -- -- Restoring Exercises Database 
+-- 
+-- We will use a new database for a set of exercise questions.
+-- This database has a public and cd schema. 
+-- This means the queries for the FROM 
+-- tables will have cd. in front of them, for example:
+-- -- SELECT * FROM cd.bookings
+-- -- SELECT * FROM cd.facilities 
+
+--
+
+SELECT * FROM cd.facilities
+
+--
+
+SELECT * FROM cd.bookings
+
+--
+
+SELECT * FROM cd.members
